41679c374618b96291a129b75db1c44a269ec3a7
Aarsh Kumar <aarshkumar.im.m85@gmail.com>
2026-02-05 11:54:42 +0530

    converting Datatypes

---
+++ .devcontainer/01_basics/03_conversionOfDatatypes.js
@@ -0,0 +1,17 @@
+let score = "100";
+console.log(typeof score);
+
+//string to number conversion
+let ValueInnumber = Number(score);
+console.log(typeof ValueInnumber);
+
+//number to null
+let newscore = null;
+console.log(typeof newscore);
+
+//string to float
+let marks = "99.99";
+console.log(typeof marks);
+let marksInFloat = parseFloat(marks);
+console.log(typeof marksInFloat);
+console.log(marksInFloat);

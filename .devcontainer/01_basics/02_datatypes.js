95adcac34cb3b9c81ad5fbeff50ab0a9aed1dd01
Aarsh Kumar <109728168+Aarsh-kumar@users.noreply.github.com>
2026-02-05 05:16:20 +0000

    learned about datatypes

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,36 @@
+"use strict"; //treat all js code as newer version(aajkl by default hi 
+// mostly newer version hi use krte)
+
+// alert (3+3) // we are using nodejs not browser
+
+console.log(3
+    +3) // code readability should be high
+
+    console.log("harwinder")
+
+    let name= "harwinder"
+
+    let age= 18
+
+    let isLoggedIn =false
+
+    console.table({name, age, isLoggedIn})
+
+    // number => 2 to the power 53
+    // bigint = for big numbers
+    // string => ""
+    // boolean =trueorfalse
+    // null = standAloneValue
+    // undefined => not assigned
+    // symbol=> unique (used in apps like figma)
+    // object= will learn further
+
+    /* console.log(typeof) // to find the types of datatype in the tags
+    */
+
+    console.log(typeof age )
+    console.log(typeof null)
+    console.log(typeof name)
+    console.log(typeof isLoggedIn )
+    
+
